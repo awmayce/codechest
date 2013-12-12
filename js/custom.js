70ae5ddb404549d4c64d5ca5d60ae3e9c0e535d3
awmayce <ayce@alphawavemedia.co.uk>
2013-12-12 19:58:49 +0000

    added fancybox on question

--- js/custom.js
+++ js/custom.js
@@ -2,5 +2,8 @@ $(document).ready(function() {
 	// Tooltips
 	$("#user .user-stats > div").tooltip({ placement: 'top' });
 	$("#information p").tooltip({ placement: 'left' });
+
+	// Fancy box
+	$(".fancybox").fancybox();
 })	
 	
